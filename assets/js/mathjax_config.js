a47e54c79806a427e11997ccb4c8b883dc809578
derut144 <derut.qe@gmail.com>
2020-03-24 20:53:04 +0900

    2020-03-24

---
+++ assets/js/mathjax_config.js
@@ -0,0 +1,13 @@
+﻿MathJax.Hub.Config{
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    displayMath: [['$$','$$'], ['\\[','\\]']],
+  }
+  Tex: {
+    Macros: {
+      C: '{\\mathbb C}',
+      R: '{\\mathbb R}',
+      Q: '{\\mathbb Q}',
+      Z: '{\\mathbb Z}'
+  }
+}
